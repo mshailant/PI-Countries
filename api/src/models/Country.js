d91151b63f86e08aa306c2ae4ca32554ff6e0f1f
msh <mshailant@gmail.com>
2022-02-03 17:17:56 -0300

    code formatted

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,43 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define("country", {
+    id: {
+      type: DataTypes.STRING(3),
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      },
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    area: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
   });
 };
