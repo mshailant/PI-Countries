47280c70b2c1d8c6960fa9d59e7e07addf03886f
msh <mshailant@gmail.com>
2022-02-03 23:42:09 -0300

    now save countries to db when server start

--- api/index.js
+++ api/index.js
@@ -19,13 +19,17 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const {
+  saveCountriesFromApi,
+} = require("./src/services/countries.services.js");
 
 // Syncing all the models at once.
 conn
   .sync({ force: true })
   .then(() => {
     server.listen(3001, () => {
-      console.log("%s listening at 3001"); // eslint-disable-line no-console
+      console.log("server listening at 3001"); // eslint-disable-line no-console
     });
   })
+  .then(async () => await saveCountriesFromApi())
   .catch((e) => console.log(e));
