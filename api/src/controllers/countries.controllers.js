f42f3f2f6b6d106feff9786dd55570723a0d39cc
msh <mshailant@gmail.com>
2022-02-07 18:17:43 -0300

    fix: now search countries by name is inside of getCountriesController

--- api/src/controllers/countries.controllers.js
+++ api/src/controllers/countries.controllers.js
@@ -6,8 +6,13 @@ const {
 
 const getCountriesController = async (req, res) => {
   try {
-    const countries = await getCountries();
-    res.json(countries);
+    if (req.query.name) {
+      const countries = await getCountriesByName(req.query.name);
+      res.status(200).json(countries);
+    } else {
+      const countries = await getCountries();
+      res.status(200).json(countries);
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
@@ -28,22 +33,7 @@ const getCountryByIdController = async (req, res) => {
   }
 };
 
-const getCountriesByNameController = async (req, res) => {
-  try {
-    const { name } = req.params;
-    const countries = await getCountriesByName(name);
-    if (!countries) {
-      throw new Error("Country not found");
-    }
-    res.json(countries);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-};
-
 module.exports = {
   getCountriesController,
   getCountryByIdController,
-  getCountriesByNameController,
 };
