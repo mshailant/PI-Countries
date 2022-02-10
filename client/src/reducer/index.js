e5c7b76114536ade20d19ee674b8235306731d5f
msh <mshailant@gmail.com>
2022-02-10 16:26:44 -0300

    added action get countries to reducer

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -3,6 +3,21 @@ const initialState = {
   country: {},
 };
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_COUNTRIES":
+      return {
+        ...state,
+        countries: action.payload,
+      };
+    case "GET_COUNTRY":
+      return {
+        ...state,
+        country: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
