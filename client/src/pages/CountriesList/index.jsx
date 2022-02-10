6f9fac2ee0cbeca8b2b8bde7867deb78c7afb5f5
msh <mshailant@gmail.com>
2022-02-10 16:27:54 -0300

    now get countries from api

--- client/src/pages/CountriesList/index.jsx
+++ client/src/pages/CountriesList/index.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { CountriesContainer, CountriesWrapper } from "./CountriesListElements";
 import CountryCard from "../../components/CountryCard";
-import { countries } from "../../data/countries";
+import { getCountries } from "../../actions";
 
 const CountriesList = () => {
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.countries);
+
+  useEffect(() => {
+    dispatch(getCountries());
+  }, [dispatch]);
+
   return (
     <>
       <CountriesContainer>
