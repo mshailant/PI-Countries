797b3598e8b5bca6da6ee8b521ba87bd138c13a0
msh <mshailant@gmail.com>
2022-02-10 15:57:26 -0300

    added countries list page

---
+++ client/src/pages/CountriesList/CountriesListElements.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const CountriesContainer = styled.div`
+  /* margin-top: 10px; */
+  /* margin-bottom: 20px; */
+  width: 100vw;
+  min-height: 100vh;
+  padding: 2rem calc((100vw - 1300px) / 2);
+  /* padding: 2rem; */
+  background: #fff;
+  color: #fff;
+`;
+
+export const CountriesWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 auto;
+`;
--- client/src/pages/CountriesList/index.jsx
+++ client/src/pages/CountriesList/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { CountriesContainer, CountriesWrapper } from "./CountriesListElements";
+import CountryCard from "../../components/CountryCard";
+import { countries } from "../../data/countries";
 
 const CountriesList = () => {
-  return <div>CountriesList</div>;
+  return (
+    <>
+      <CountriesContainer>
+        <CountriesWrapper>
+          {countries.map((country) => (
+            <CountryCard key={country.id} country={country} />
+          ))}
+        </CountriesWrapper>
+      </CountriesContainer>
+    </>
+  );
 };
 
 export default CountriesList;
