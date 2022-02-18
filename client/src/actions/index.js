5a2c80b735d2775d831e31ba09c4318e7384f660
msh <mshailant@gmail.com>
2022-02-18 14:55:50 -0300

    added new action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,16 +12,67 @@ export const getCountries = () => {
   };
 };
 
-export const filterCountries = (payload) => {
+export const filterCountries = (payload, isSearch) => {
+  console.log(isSearch);
   return {
     type: "FILTER_COUNTRIES",
     payload,
+    isSearch,
   };
 };
 
-export const sortCountries = (payload) => {
+export const sortCountries = (payload, isSearch) => {
   return {
     type: "SORT_COUNTRIES",
     payload,
+    isSearch,
+  };
+};
+
+export const searchCountries = (payload, isSearch) => {
+  return async (dispatch) => {
+    const response = await axios.get(`${API_URL}countries?name=${payload}`);
+    dispatch({
+      type: "SEARCH_COUNTRIES",
+      payload: response.data,
+      isSearch,
+    });
+  };
+};
+
+export const createActivity = (payload) => {
+  console.log("action", payload);
+  let activity = {
+    activity: {
+      name: payload.name,
+      difficulty: payload.difficulty,
+      duration: payload.duration,
+      season: Array.from(payload.season),
+    },
+    countries: Array.from(payload.countries),
+  };
+
+  return async (dispatch) => {
+    const response = await axios.post(`${API_URL}activity`, activity);
+    dispatch({
+      type: "CREATE_ACTIVITY",
+      payload: response.data,
+    });
+  };
+};
+
+export const resetSearch = () => {
+  return {
+    type: "RESET_SEARCH",
+  };
+};
+
+export const getCountry = (payload) => {
+  return async (dispatch) => {
+    const response = await axios.get(`${API_URL}countries/${payload}`);
+    dispatch({
+      type: "GET_COUNTRY",
+      payload: response.data,
+    });
   };
 };
