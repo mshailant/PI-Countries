a1546d2bef8c24d1d1ee0ed5ee3b959cbb0a4005
msh <mshailant@gmail.com>
2022-02-18 14:55:00 -0300

    added link to details

--- client/src/components/CountryCard/index.jsx
+++ client/src/components/CountryCard/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Card, CardImg, CardH1, CardH2 } from "./CountryCardElements";
+import { Card, CardImg, CardH1, CardH2, CardLink } from "./CountryCardElements";
 
 const CountryCard = ({ country }) => {
   return (
     <>
-      <Card>
-        <CardImg src={country.image} />
-        <CardH1>{country.name}</CardH1>
-        <CardH2>{country.continent}</CardH2>
-      </Card>
+      <CardLink to={`/countries/${country.id}`}>
+        <Card>
+          <CardImg src={country.image} />
+          <CardH1>{country.name}</CardH1>
+          <CardH2>{country.continent}</CardH2>
+        </Card>
+      </CardLink>
     </>
   );
 };
